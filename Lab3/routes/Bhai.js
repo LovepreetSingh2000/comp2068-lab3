246d3d1e8dcbd0058b76568d348d712f5ed17231
Lovepreet Singh <200395873@student.georgianc.on.ca>
2019-10-02 23:28:55 -0400

    Add project files.

---
+++ Lab3/routes/Bhai.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('Bhai', { title: 'Bhai', comment: 'My bhai is 20 year old' });
+});
+
+module.exports = router;
